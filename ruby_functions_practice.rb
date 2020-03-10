22b2988f14cfe86e751106348c19a3a29810b00b
Gordon Soutter <gordon.soutter@outlook.com>
2020-03-10 16:19:02 +0000

    Lab intial commit, for sharing. With month name bodge

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,44 @@
+
+
+def return_10()
+  return 10
+end
+
+
+def add( n1, n2 )
+  return n1 + n2
+end
+
+def subtract( n1, n2 )
+  return n1 - n2
+end
+
+def multiply( n1, n2 )
+  return n1 * n2
+end
+
+def divide( n1, n2 )
+  return n1 / n2
+end
+
+def length_of_string( test_string )
+  return test_string.length
+end
+
+def join_string( string_1, string_2 )
+  return string_1 + string_2
+end
+
+def add_string_as_number( string_1, string_2)
+  return string_1.to_i + string_2.to_i
+end
+
+def number_to_full_month_name(month_number)
+  months = { 1 => "January", 2 => "February", 3 => "March", 4 => "April", 5 => "May", 6 => "June", 7 => "July", 8 => "August", 9 => "September", 10 => "October", 11 => "November", 12 => "Decemeber"}
+  return months[month_number]
+end
+
+def number_to_short_month_name(month_number)
+  months = { 1 => "January", 2 => "February", 3 => "March", 4 => "April", 5 => "May", 6 => "June", 7 => "July", 8 => "August", 9 => "September", 10 => "October", 11 => "November", 12 => "Decemeber"}
+  return months[month_number][0,3]
+end
