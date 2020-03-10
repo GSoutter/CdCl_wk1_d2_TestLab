3a90c1bcfa18d269655a4d906a1452b9b2a7bc5e
Craig Barratt <craigtbarratt@gmail.com>
2020-03-10 19:09:58 +0000

    removes comments from further exercises

--- specs/ruby_functions_spec.rb
+++ specs/ruby_functions_spec.rb
@@ -79,25 +79,19 @@ class FunctionsTest < MiniTest::Test
     assert_equal( "Oct", tenth_month_string )
   end
 
-  #Further
-
-  #Given the length of a side of a cube calculate the volume
   def test_volume_of_cube()
     test_volume = volume_of_cube ( 2 )
     assert_equal(8, test_volume)
   end
 
-  #Given the radius of a sphere calculate the volume
   def test_volume_of_sphere()
     test_volume = volume_of_sphere ( 3 )
     assert_equal(84.78, test_volume)
   end
 
-  #Given a value in farenheit, convert this into celsius.
   def test_fahrenheit_to_celsius()
     test_conversion = conversion ( 74 )
-    assert_equal(23.33, test_conversion)  
+    assert_equal(23.33, test_conversion)
   end
-
 
 end
